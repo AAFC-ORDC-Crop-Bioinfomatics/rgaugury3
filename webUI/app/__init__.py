75c567a9f643bc3905cae94853b2737b57d0230f
vincent.quan <alburt@qq.com>
2016-06-20 14:42:55 -0500

    fix a bug in status page. when there is no projects folder then the server will crash

--- webUI/app/__init__.py
+++ webUI/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from os.path import abspath,dirname
+import logging
+from config import WEB_LOG
+
+logging.basicConfig(filename=WEB_LOG,level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config['PROGRAM_HOME'] = dirname(abspath(__file__))
--- webUI/startPipeline.py
+++ webUI/startPipeline.py
@@ -2,17 +2,14 @@
 import sys
 from subprocess import Popen, PIPE
 import argparse
-from app import app, db, models
+from app import app, db, models, logging
 from psutil import Process
 from config import PRJ_HOME, META_FILE,FASTA_EXTENSION,GENE_FILE, LOG_FILE
-from config import TOTAL_STEPS,DATE_FORMAT, FINAL_STATUS,PENDING, WEB_LOG
+from config import TOTAL_STEPS,DATE_FORMAT, FINAL_STATUS,PENDING
 from app.tool import countType, initGeneImage, initGeneSection,coutInputAmount, getCPU
 from datetime import datetime
 from os.path import exists
 import re
-import logging
-
-logging.basicConfig(filename=WEB_LOG,level=logging.DEBUG)
 
 command = sys.argv[1:14]
 
